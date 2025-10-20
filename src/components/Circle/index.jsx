f812552a7e0d3e87baed0c267219a5da2661d724
Neil Kirsopp <bleh@kirsopp.me>
2025-10-20 15:02:50 +0200

    Circle, Ellipse, Polygon and Rectangle all have sensible bounds now

--- src/components/Circle/index.jsx
+++ src/components/Circle/index.jsx
@@ -1,15 +1,23 @@
 import { useAppData } from '../../hooks';
 import { handleMouseDoubleClick, handleMouseDown, handleMouseEnter, handleMouseLeave, handleMouseMove, handleMouseUp, handleMouseWheel, parseFlexStyles, rgbColor } from '../../utils';
+
 const Circle = ({ data }) => {
-  const parentSize = JSON.parse(localStorage.getItem('formDimension'));
-  const { FillCol, Start, FCol, Points, Radius, Event, CSS } = data?.Properties;
-  const customStyles = parseFlexStyles(CSS)
-const {socket} = useAppData()
+  const { FillCol, Start, FCol, Points, Radius, Event, CSS, Visible } = data?.Properties;
+  const customStyles = parseFlexStyles(CSS);
+  const { socket } = useAppData();
+
+  const cx = Points && Points[1][0];
+  const cy = Points && Points[0][0];
+  const rx = Radius && Radius[0];
+  const ry = Radius && Radius[0];
+
+  // Calculate bounding box
+  const minX = cx - rx;
+  const minY = cy - ry;
+  const width = rx * 2;
+  const height = ry * 2;
+
   const generatePieChartPaths = (startAngles) => {
-    const cx = Points && Points[1][0];
-    const cy = Points && Points[0][0];
-    const rx = Radius && Radius[0];
-    const ry = Radius && Radius[0];
 
     const paths = [];
 
@@ -41,39 +49,49 @@ const {socket} = useAppData()
   };
 
   const paths = generatePieChartPaths(Start);
- 
+
   return (
     <div
       style={{
         position: 'absolute',
-        top: 0,
-        left: 0,
-        pointerEvents: 'none',
-        ...customStyles
-      }}
-      onMouseDown={(e) => {
-        handleMouseDown(e, socket, Event,data?.ID);
-      }}
-      onMouseUp={(e) => {
-        handleMouseUp(e, socket, Event, data?.ID);
-      }}
-      onMouseEnter={(e) => {
-        handleMouseEnter(e, socket, Event, data?.ID);
-      }}
-      onMouseMove={(e) => {
-        handleMouseMove(e, socket, Event, data?.ID);
-      }}
-      onMouseLeave={(e) => {
-        handleMouseLeave(e, socket, Event, data?.ID);
-      }}
-      onWheel={(e) => {
-        handleMouseWheel(e, socket, Event, data?.ID);
-      }}
-      onDoubleClick={(e)=>{
-        handleMouseDoubleClick(e, socket, Event,data?.ID);
+        display: Visible == 0 ? 'none' : 'block',
       }}
     >
-      <svg height={parentSize && parentSize[0]} width={parentSize && parentSize[1]}>
+      <svg
+        id={data?.ID}
+        viewBox={`${minX} ${minY} ${width} ${height}`}
+        style={{
+          position: 'absolute',
+          top: `${minY}px`,
+          left: `${minX}px`,
+          pointerEvents: 'auto',
+          overflow: 'visible',
+          ...customStyles,
+        }}
+        height={height}
+        width={width}
+        onMouseDown={(e) => {
+          handleMouseDown(e, socket, Event, data?.ID);
+        }}
+        onMouseUp={(e) => {
+          handleMouseUp(e, socket, Event, data?.ID);
+        }}
+        onMouseEnter={(e) => {
+          handleMouseEnter(e, socket, Event, data?.ID);
+        }}
+        onMouseMove={(e) => {
+          handleMouseMove(e, socket, Event, data?.ID);
+        }}
+        onMouseLeave={(e) => {
+          handleMouseLeave(e, socket, Event, data?.ID);
+        }}
+        onWheel={(e) => {
+          handleMouseWheel(e, socket, Event, data?.ID);
+        }}
+        onDoubleClick={(e) => {
+          handleMouseDoubleClick(e, socket, Event, data?.ID);
+        }}
+      >
         {paths.map((path, index) => (
           <path
             key={index}
