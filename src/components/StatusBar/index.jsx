b395a17f6e161357e31f1330a5ae344305a12275
Aarush Bhat <aarush.bhatt@gmail.com>
2025-10-28 17:53:28 +0530

    add bcol,fcol,fontobj props to statusbar and statusfield

--- src/components/StatusBar/index.jsx
+++ src/components/StatusBar/index.jsx
@@ -1,23 +1,32 @@
-import { excludeKeys } from "../../utils";
+import { excludeKeys, parseFlexStyles, getFontStyles, rgbColor } from "../../utils";
 import StatusField from "../StatusField";
+import { useAppData } from "../../hooks";
 
 const StatusBar = ({ data }) => {
-    const { Align = 'Bottom', Posn, Size } = data?.Properties;
+    const { Align = 'Bottom', Posn, Size, CSS } = data?.Properties;
 
     if (Align != 'Bottom') {
         console.error("StatusBar with Align other than Bottom is not supported yet.");
     }
+    const { findCurrentData, inheritedProperties } = useAppData();
+    const { BCol, FCol, FontObj } = inheritedProperties(data, 'BCol', 'FCol', 'FontObj');
+    const font = findCurrentData(FontObj);
+    const fontStyles = getFontStyles(font, 12);
+
     const styles = {
         position: 'absolute',
         display: 'flex',
         alignItems: 'left',
         padding: '0 8px',
-        backgroundColor: '#f0f0f0',
+        backgroundColor: BCol,
+        color: FCol,
         borderTop: '1px solid #ccc',
         bottom: Posn?.bottom || 0,
         left: 0,
         right: 0,
         height: Size?.height || '22px',
+        ...fontStyles,
+        ...parseFlexStyles(CSS)
     };
     return (
         <div id={data?.ID} style={styles}>
--- src/components/StatusField/index.jsx
+++ src/components/StatusField/index.jsx
@@ -1,16 +1,22 @@
-const StatusField = ({ data }) => {
-    const { Caption, Text, Posn, Size } = data?.Properties;
+import { parseFlexStyles, getFontStyles, rgbColor } from "../../utils";
+import { useAppData } from "../../hooks";
 
-    /*TODO:
-    - fontobj
-    - fcol
-    - bcol
-    */
+const StatusField = ({ data }) => {
+    const { Caption, Text, Posn, Size, CSS } = data?.Properties;
+
+    const { findCurrentData, inheritedProperties } = useAppData();
+    const { BCol, FCol, FontObj } = inheritedProperties(data, 'BCol', 'FCol', 'FontObj');
+
+    const font = findCurrentData(FontObj);
+    const fontStyles = getFontStyles(font, 12);
 
     const styles = {
         padding: '0 16px 0 4px',
         borderRight: '1px solid #ccc',
-        backgroundColor: '#f0f0f0',
+        backgroundColor: BCol,
+        color: FCol,
+        ...fontStyles,
+        ...parseFlexStyles(CSS)
     };
     return (
         <div id={data?.ID} style={styles}>
